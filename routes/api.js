e274d7f1c5769770e6746d1a860f23a1c42543ca
rivermanvr <vrgithub@riversconsulting.com>
2017-10-04 00:16:49 -0400

    empty folder/file skeleton created

---
+++ routes/api.js
@@ -0,0 +1,10 @@
+const express = require( 'express' );
+const router = express.Router();
+
+// const studentRoutes = require( './student_routes' );
+// const campusRoutes = require( './campus_routes' );
+
+// router.use('/students', studentRoutes);
+// router.use('/campuses', campusRoutes);
+
+module.exports = router;
