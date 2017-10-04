9d981a35fff1d1d5c503940491537d34fd4b7ae0
rivermanvr <vrgithub@riversconsulting.com>
2017-10-04 13:07:59 -0400

    added seed to server.js

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ const chalk = require( 'chalk' );
 
 const port = process.env.PORT || 2020;
 
-// db.seed()
-db.sync()
+db.seed()
 .then(() => server.listen(port, () => console.log(chalk.yellow(`listening on port ${ port }`))))
 .catch(err => {
   console.error(chalk.red('Error in sync/seed'));
