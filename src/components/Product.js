14deb895db8096fd6f3087951352302d167c9dba
AJ Frank <alexanderjfrank@gmail.com>
2017-10-05 15:22:21 -0400

    adding some more details to product

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,12 +7,16 @@ export default function Product(props) {
     <div className="col-md-3 col-xs-6">
       <Link to={`/product/${id}`} className="thumbnail">
         <img src={image} />
-        <div className="caption">
-          <h3>{title}</h3>
-          <p>{description}</p>
-          <p>Price: {price}</p>
-        </div>
       </Link>
+      <div className="caption">
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <p>Price: {price}</p>
+        <p>
+          Select Quantity <input />
+        </p>
+        <button className="btn btn-default">Add to cart </button>
+      </div>
     </div>
   );
 }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -22,11 +22,28 @@ export default function ProductList(props) {
       image: "https://d3nevzfk7ii3be.cloudfront.net/igi/ipv5OG2NckM3DfE2.large"
     }
   ];
+
+  const categories = ["phone", "tablet", "laptop"];
+
   return (
     <div className="col-sm-8">
       <div className="row">
         <h1>Products</h1>
+        <br />
       </div>
+      <div>
+        <h4> Select Category:</h4>
+        <select>
+          {categories.map(category => (
+            <option key={category}>{category}</option>
+          ))}
+        </select>
+      </div>
+      <br />
+      <div>
+        Search: <input />
+      </div>
+      <br />
       <div className="row">
         {productList.map(product => {
           return <Product key={product.id} product={product} />;
