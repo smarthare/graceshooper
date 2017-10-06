6a68b17b3e4cc5699768359e6ffd711abd9405a2
smauditoria <svob9263@gmail.com>
2017-10-05 23:05:49 -0400

    moved back

---
+++ server/api/product/product.model.js
@@ -0,0 +1,38 @@
+const conn = require('../../conn')
+
+const Product = conn.define('product', {
+  title: {
+    type: conn.Sequelize.STRING,
+    allowNull: false,
+    unique: true,
+    validate: {
+      notEmpty: true
+    }
+  },
+  description: {
+    type: conn.Sequelize.TEXT,
+    defaultValue: 'Awesome product!'
+  },
+  price: {
+    type: conn.Sequelize.FLOAT,
+    defaultValue: 0,
+    validate: {
+      min: 0
+    }
+  },
+  inventory: {
+    type: conn.Sequelize.INTEGER,
+    defaultValue: 0,
+    validate: {
+      min: 0
+    }
+  },
+  imgUrls: {
+    type: conn.Sequelize.ARRAY(conn.Sequelize.STRING),
+    validate: {
+      isUrl: true
+    }
+  }
+})
+
+module.exports = Product
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require( 'path' );
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['stage-2', 'react', 'env']
+        }
+      }
+    ]
+  }
+};
