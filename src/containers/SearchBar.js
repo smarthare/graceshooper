554ed393b61b7cb0c97c751ade46a5daa452b0cb
rivermanvr <vrgithub@riversconsulting.com>
2017-10-08 22:22:15 -0400

    final refactoring of searchBar

--- src/containers/SearchBar.js
+++ src/containers/SearchBar.js
@@ -42,6 +42,7 @@ class SearchBar extends Component {
 
   handleSubmit(event) {
     event.preventDefault();
+    this.props.router.history.push('/');
     this.props.writeSearchTerm(this.state.searchTerm, this.state.searchCategory);
   }
 
