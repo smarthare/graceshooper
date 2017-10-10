a1a1ef1fd547e8aac6b9c87bfd0b9db51c046bd1
smauditoria <svob9263@gmail.com>
2017-10-10 18:14:11 -0400

    added order, auth, crated checkout

--- server/api/auth/auth.route.js
+++ server/api/auth/auth.route.js
@@ -5,20 +5,20 @@ const
 router
   .get('/', (req, res, next) => {
     User.findBySessionId(req.session.userId)
-    .then(res.status(200).json)
+    .then(user => res.send(user))
     .catch(next)
   })
 
   .delete('/', (req, res, next) => {
     req.session.destroy()
-    .res.sendStatus(204)
+    res.sendStatus(204)
   })
 
   .post('/', (req, res, next) => {
     User.login(req.body)
     .then(user => {
       req.session.userId = user.id
-      res.status(200).json(user)
+      res.send(user)
     })
     .catch(next)
   })
