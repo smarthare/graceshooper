488e609538b11710c5f26b3f7b49552e735142e2
rivermanvr <vrgithub@riversconsulting.com>
2017-10-11 20:41:42 -0400

    all category main body links work

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -3,22 +3,70 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 class Home extends Component {
-  constructor() {
-    super();
-    this.state = {}
+  constructor(props) {
+    super(props);
+    this.state = {
+      pathname: '/',
+      categoryId: 0,
+      term: '',
+      filter: false
+    }
   }
 
   componentWillReceiveProps(nextProps) {
+    /*********************************************/
+    // console.log - delete at some point
     console.log('>>>>>>>>>receiving Props:');
     console.log('>>>This Props:', this.props);
     console.log('>>>Next Props:', nextProps);
-    
+    /*********************************************/
+    // get nextProps variables
+    const pathnameLast = this.state.pathname;
+    const pathname = nextProps.router.location.pathname;
+    const categoryId = (nextProps.router.match.params.id) ? nextProps.router.match.params.id * 1 : 0;
+    const term = (nextProps.router.match.params.term) ? nextProps.router.match.params.term : '';
+    /*********************************************/
+    // checking to see if someone clicked a link and changed the url
+    // if yes, then lets see what we see...
+    if (pathname !== pathnameLast) this.setState({ pathname, categoryId, term, filter: true })
   }
 
   render() {
-    console.log('********** HOME - this.props: ', this.props)
+    /*********************************************/
+    // setup local variables
+    let products = this.props.products;
     const categories = this.props.categories;
-    const products = this.props.products;
+    const filter = this.state.filter;
+    const categoryId = this.state.categoryId;
+    const term = this.state.term;
+    /*********************************************/
+    //if ( !categories.length && !products.length) return <div></div>
+
+    console.log('********** HOME - this.props: ', this.props)
+    console.log('>>>>>>>>>>filter: ', this.state);
+
+    /*********************************************/
+    // filter the Products List? - Main Section
+    // are we filtering by category?
+    if (filter && categoryId) {
+       products = products.filter(product => {
+        let acceptProd = false;
+        product.categories.forEach(category => {
+          if (categoryId === category.id) acceptProd = true;
+        })
+        return acceptProd;
+      })
+    }
+    // now check if we are filtering by search term.
+    // if (filter && term) {
+    //   products = products.filter(product => {
+    //    let acceptProd = false;
+    //    product.categories.forEach(category => {
+    //      if (categoryId === category.id) acceptProd = true;
+    //    })
+    //    return acceptProd;
+    //  })
+    // }
 
     /*********************************************/
     // create the Categories List - Sidebar
