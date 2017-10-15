d54acc9f68a4683fda22fafd4c7ee3988a85de8d
ajfrank <alexanderjfrank@gmail.com>
2017-10-15 09:19:43 -0400

    refactor

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -4,44 +4,34 @@ import store from "../store";
 import Categories from "./Categories";
 import Users from "./Users";
 import ProductDisplay from "./ProductDisplay";
-import {Link, Route, NavLink} from 'react-router-dom'
+import { Link, Route, NavLink } from "react-router-dom";
 
-function Admin(props) {
+export default function Admin(props) {
   return (
     <div>
       <h1>Admin</h1>
       <ul className="nav nav-tabs">
         <li>
-          <NavLink to='/admin/categories' activeClassName="selected">
+          <NavLink to="/admin/categories" activeClassName="selected">
             CATEGORIES
           </NavLink>
         </li>
         <li>
-          <NavLink to='/admin/users' activeClassName="selected">
+          <NavLink to="/admin/users" activeClassName="selected">
             USERS
           </NavLink>
         </li>
         <li>
-          <NavLink to='/admin/products' activeClassName="selected">
+          <NavLink to="/admin/products" activeClassName="selected">
             PRODUCTS
           </NavLink>
         </li>
       </ul>
       <div>
-        <Route path='/admin/categories' component={Categories} />
-        <Route path='/admin/users' component={Users} />
-        <Route path='/admin/products' component={ProductDisplay} />
+        <Route path="/admin/categories" component={Categories} />
+        <Route path="/admin/users" component={Users} />
+        <Route path="/admin/products" component={ProductDisplay} />
       </div>
     </div>
   );
 }
-
-const mapStateToProps = state => {
-  return {};
-};
-
-const mapDispatchToProps = dispatch => {
-  return {};
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Admin);
